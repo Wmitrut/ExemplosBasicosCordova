c480e3c92ae6b172e433e869150b3158d3eb82ed
Guilherme Berghauser <tortoyoyo@gmail.com>
2014-10-11 18:04:27 -0300

    Exemplos Básicos de cordova para estudo

---
+++ floripaNativo/platforms/firefoxos/www/cordova_plugins.js
@@ -0,0 +1,103 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/src/firefoxos/notification.js",
+        "id": "org.apache.cordova.dialogs.dialogs-impl",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.battery-status/www/battery.js",
+        "id": "org.apache.cordova.battery-status.battery",
+        "clobbers": [
+            "navigator.battery"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.battery-status/src/firefoxos/BatteryProxy.js",
+        "id": "org.apache.cordova.battery-status.Battery",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/src/firefoxos/DeviceProxy.js",
+        "id": "org.apache.cordova.device.DeviceProxy",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/src/firefoxos/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer-impl",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/CompassError.js",
+        "id": "org.apache.cordova.device-orientation.CompassError",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/CompassHeading.js",
+        "id": "org.apache.cordova.device-orientation.CompassHeading",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/compass.js",
+        "id": "org.apache.cordova.device-orientation.compass",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/src/firefoxos/compass.js",
+        "id": "org.apache.cordova.device-orientation.compass-impl",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.dialogs": "0.2.10",
+    "org.apache.cordova.battery-status": "0.2.11",
+    "org.apache.cordova.device": "0.2.12",
+    "org.apache.cordova.device-motion": "0.2.10",
+    "org.apache.cordova.device-orientation": "0.3.9",
+    "org.apache.cordova.splashscreen": "0.3.3"
+}
+// BOTTOM OF METADATA
+});
---
+++ floripaNativo/platforms/firefoxos/www/js/index.js
@@ -0,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        alert('entrou');
+
+
+        //NOTIFICAÇÃO ---------------
+
+        // navigator.notification.alert(
+        //     'You are the winner!', 
+        //     function() {
+        //         console.log('Ok');
+        //     }, 
+        //     'Game Over', // title
+        //     'Done' // buttonName
+        // );
+
+        //NOTIFICAÇÃO ---------------
+
+        //Verificar quantidade de bateria
+        // e se esta plugada na energia
+        // ainda tem o evento batterycritical e batterylow
+
+        // window.addEventListener("batterystatus", onBatteryStatus, false);
+        // function onBatteryStatus(info) {
+        //     // Handle the online event
+        //     console.log("% da bateria: " + info.level + " Carregando? " + info.isPlugged);
+        // }
+
+        //CONTATOS ------------------
+
+        //ultima versao dando pau!
+
+        //DEVICE ----------------
+
+        // console.log('Cordova: ' + device.cordova + ' Plataforma: ' + device.platform);
+
+
+        //ACCELEROEMETER ------------
+
+        //FUNÇÃO QUE PEGA POSIÇÃO
+
+        // function onSuccess(acceleration) {
+        //     alert('Acceleration X: ' + acceleration.x + '\n' +
+        //         'Acceleration Y: ' + acceleration.y + '\n' +
+        //         'Acceleration Z: ' + acceleration.z + '\n' +
+        //         'Timestamp: ' + acceleration.timestamp + '\n');
+        // };
+
+        // function onError() {
+        //     alert('onError!');
+        // };
+
+        // navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+
+        //FUNCAO QUE PEGA POR TEMPO 
+
+        // function onSuccess(acceleration) {
+        //     alert('Acceleration X: ' + acceleration.x + '\n' +
+        //         'Acceleration Y: ' + acceleration.y + '\n' +
+        //         'Acceleration Z: ' + acceleration.z + '\n' +
+        //         'Timestamp: ' + acceleration.timestamp + '\n');
+        // };
+
+        // function onError() {
+        //     alert('onError!');
+        // };
+
+        // var options = {
+        //     frequency: 3000
+        // }; // Update every 3 seconds
+
+        // var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+
+
+        //DEVICE ORIETATION -----------
+
+        // function onSuccess(heading) {
+        //     alert('Heading: ' + heading.magneticHeading);
+        // };
+
+        // function onError(error) {
+        //     alert('CompassError: ' + error.code);
+        // };
+
+        // navigator.compass.getCurrentHeading(onSuccess, onError);
+
+        //InAppBrowser
+
+        // var ref = window.open('teste.html', '_self', 'location=yes');
+
+        //SPLASHSCREEN
+        navigator.splashscreen.show();
+        setTimeout(function() {
+            navigator.splashscreen.hide();
+        }, 2000);
+
+    }
+};
